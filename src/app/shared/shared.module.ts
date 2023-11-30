f65d9e70a4b45054b7238e948b537851c8dd0dfe
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2023-11-30 14:15:47 -0600

    FEATS_dash_profile

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TrackerComponent } from './tracker/tracker.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TrackerComponent
+  ],
   imports: [
     CommonModule
   ]
