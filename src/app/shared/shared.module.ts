628118d643584b6bcfd72fddd27ed92e9a501eaf
Topaz712 <tzam712@gmail.com>
2023-11-30 15:39:04 -0600

    CONFIG- ng circle progress

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TrackerComponent } from './tracker/tracker.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 
 @NgModule({
@@ -13,14 +14,32 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgCircleProgressModule.forRoot({
+      "radius": 60,
+      "space": -10,
+      "outerStrokeGradient": true,
+      "outerStrokeWidth": 10,
+      "outerStrokeColor": "#f37c7c",
+      "outerStrokeGradientStopColor": "#f37c7c",
+      "innerStrokeColor": "#f2edf0",
+      "innerStrokeWidth": 10,
+      "title": "Kcal",
+      titleColor: "#f37c7c",
+      "animateTitle": false,
+      "animationDuration": 1000,
+      "showUnits": false,
+      "showBackground": false,
+      "startFromZero": false,
+      "lazy": true}),
   ],
   exports: [
     TrackerComponent,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgCircleProgressModule
   ]
 })
 export class SharedModule { }
