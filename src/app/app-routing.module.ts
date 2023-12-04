9b6813903667cd7ac7ea1a7c1e79efcd472229bd
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2023-12-04 11:55:15 -0600

    BUG_trying to route signup
    I am trying to add the child routes to the auth in the app-routing in order to display the signup when I serve this project...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
+  {
+    path: 'signup',
+    loadChildren: () => import('./auth/pages/signup/signup.module').then ((m) => m. SignupModule),
+  },
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
- 
+
 ];
 
 @NgModule({
---
+++ src/app/auth/pages/signup/signup.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from "@angular/common";
+import { AppRoutingModule } from "src/app/app-routing.module";
+import { SharedModule } from "src/app/shared/shared.module";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  declarations: [],
+  imports:
+  [CommonModule,
+  AppRoutingModule,
+  SharedModule]
+})
+export class SignupModule{}
