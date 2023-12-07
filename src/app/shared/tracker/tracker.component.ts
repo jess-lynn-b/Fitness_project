10b5f3114890eb48b15539cdbd4ba44aa3cf292c
Millisa G <135849424+millisag@users.noreply.github.com>
2023-12-07 12:17:27 -0500

    Added-HTML

--- src/app/shared/tracker/tracker.component.ts
+++ src/app/shared/tracker/tracker.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class TrackerComponent {
 
+  height: number = 0;
+  weight: number = 0;
+  targetWeight: number = 0;
+  estimatedTime: number = 0;
+
+  calculateTime() {
+    
+    const weightToLose = this.weight - this.targetWeight;
+    const averageWeightLossPerWeek = 0.5; // Adjust as needed
+    this.estimatedTime = weightToLose / averageWeightLossPerWeek;
+  }
 }
