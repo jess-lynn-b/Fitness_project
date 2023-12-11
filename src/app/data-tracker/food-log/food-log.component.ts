bb722a07145833f42c23d1549ed0fa523f63e6d4
Topaz712 <tzam712@gmail.com>
2023-12-11 13:09:39 -0600

    ADD: add food buttons

--- src/app/data-tracker/food-log/food-log.component.ts
+++ src/app/data-tracker/food-log/food-log.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,5 +9,8 @@ import { Component } from '@angular/core';
 })
 
 export class FoodLogComponent {
+
+  constructor(
+    private router: Router) { }
 
 }
