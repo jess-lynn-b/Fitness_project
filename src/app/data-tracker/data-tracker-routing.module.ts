09bda79d016d21be1ba5d602192400ceb1d0a19b
Topaz712 <tzam712@gmail.com>
2023-12-10 23:08:04 -0600

    ADD: Lazy loading for data-tracker child routes

--- src/app/data-tracker/data-tracker-routing.module.ts
+++ src/app/data-tracker/data-tracker-routing.module.ts
@@ -2,8 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataTrackerComponent } from './data-tracker.component';
 import { FoodLogComponent } from './food-log/food-log.component';
+import { ActivityTrackerComponent } from './activity-tracker/activity-tracker.component';
+import { TrackerComponent } from './tracker/tracker.component';
 
-const routes: Routes = [{ path: '', component: DataTrackerComponent, children: [{path: 'food-log', component: FoodLogComponent }]}];
+const routes: Routes = [
+  {
+    path: '',
+    component: DataTrackerComponent, children: [
+      { path: 'food-log', component: FoodLogComponent },
+      { path: 'activity-tracker', component: ActivityTrackerComponent },
+      { path: 'tracker', component: TrackerComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
