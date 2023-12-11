99e210ae98bd1a1e88fbb0a3d46c12c6b27106bf
Topaz712 <tzam712@gmail.com>
2023-12-11 14:45:38 -0600

    nothing new

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
 
   sampleUserimg = 'https://images.pexels.com/photos/18554689/pexels-photo-18554689/free-photo-of-a-street-at-night-photographed-in-long-exposure.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load';
 }
