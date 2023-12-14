9cec3d383c6c26dc868c98c50c554c216bc46675
Millisa G <135849424+millisag@users.noreply.github.com>
2023-12-14 12:42:21 -0500

    ADDED: activity tracker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { CaloricIntakeComponent } from './caloric-intake/caloric-intake.component';
 
 
 
@@ -10,6 +11,7 @@ import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
+    CaloricIntakeComponent,
   ],
   imports: [
     BrowserModule,
